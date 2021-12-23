ea412924eb3874d83a49f17afc0b6e85982738af
Karan Kadam <karank@outlook.com>
2021-12-24 01:03:00 +1030

    CORS issue fix

--- app/main.py
+++ app/main.py
@@ -4,8 +4,29 @@ from fastapi import FastAPI
 from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:5000",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 covscrape = CovidScraper()
 
@@ -18,5 +39,3 @@ async def get_numbers(state):
 @app.get("/")
 async def get_root():
     return FileResponse("app/index.html")
-
-
